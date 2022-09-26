a16a3ff7d858be567901ea0faaeb2127690e53f9
grififth <stevez9101@gmail.com>
2022-09-25 22:32:18 -0400

    ok

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar-logo">
+                <img src="src/images/logo.png" />
+                <h1>Moco Hack Day</h1>
+            </div>
+            <div className="navbar-links">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Sponsors</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
